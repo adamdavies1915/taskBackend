a0eb850370780e34199a80231ab43f3f9c035999
Adam Davies <adam.davies@pepperhq.com>
2020-05-11 17:11:13 +0100

    formatting

--- src/routes/track.ts
+++ src/routes/track.ts
@@ -1,29 +1,30 @@
-import express from 'express'
-import { getTrack, getTracksFromArtist, getAllTracks } from '../controllers/trackController';
-import { isString, isNumber } from '../utils/utils';
+import express from "express";
+import {
+  getTrack,
+  getTracksFromArtist,
+  getAllTracks,
+} from "../controllers/trackController";
+import { isString, isNumber } from "../utils/utils";
 
 export const trackRouter = express.Router();
 
-trackRouter.get('/:id', (req, res, next) => {
-    const trackId =  Number(req.params.id);
-    if (isNumber(trackId) && !isNaN(trackId)) {
-        res.send(getTrack(trackId));
-    }
-    else {
-        res.sendStatus(400)
-    }
+trackRouter.get("/:id", (req, res, next) => {
+  const trackId = Number(req.params.id);
+  if (isNumber(trackId) && !isNaN(trackId)) {
+    res.send(getTrack(trackId));
+  } else {
+    res.sendStatus(400);
+  }
 });
 
-trackRouter.get('/',  (req, res, next) => {
-    const artist = req.query.artist;
-    if ( artist === {} || !artist ) {
-        res.send(getAllTracks()); 
-    }
-    if (isString(artist) && artist !== '') {
-        res.send(getTracksFromArtist(artist));
-    }
-    else {
-        res.sendStatus(400)
-    }
+trackRouter.get("/", (req, res, next) => {
+  const artist = req.query.artist;
+  if (artist === {} || !artist) {
+    res.send(getAllTracks());
+  }
+  if (isString(artist) && artist !== "") {
+    res.send(getTracksFromArtist(artist));
+  } else {
+    res.sendStatus(400);
+  }
 });
-
--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -3,7 +3,6 @@ const supertest = require("supertest");
 const request = supertest(app);
 
 it("Gets the test endpoint", async (done) => {
-  // Sends GET Request to /test endpoint
   const response = await request.get("/track/5");
   expect(response.status).toBe(200);
   expect(response.body).toMatchObject({
